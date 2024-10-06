cbbdd4382009012579de0497de3b530189f9af82
LightAgents <am26339@gmail.com>
2024-10-06 21:06:53 +0300

    Implemented ROS

--- TimedEncoder.h
+++ TimedEncoder.h
@@ -1,48 +1,61 @@
 #ifndef TIMEDENCODER_H
 #define TIMEDENCODER_H
 
+#include <Arduino.h>
 #include "Encoder.h"
 
 class TimedEncoder : public Encoder {
 private:
-    TIM_TypeDef *timer;
-    int32_t position;
+    volatile int32_t position;  // Current position of the encoder
+    int32_t lastPosition;       // Last recorded position for speed calculation
+    unsigned long lastTime;     // Last time speed was calculated
+    float speed;                // Speed of the encoder (pulses per second)
+    uint8_t pinA, pinB;         // Encoder pins
+    uint32_t timerInterval;     // Timer interval in milliseconds for speed calculation
 
 public:
-    // Constructor
-    TimedEncoder(TIM_TypeDef *timerInstance) : timer(timerInstance), position(0) {}
+    TimedEncoder(uint8_t encoderPinA, uint8_t encoderPinB, uint32_t interval = 100) 
+        : pinA(encoderPinA), pinB(encoderPinB), position(0), lastPosition(0), lastTime(0), speed(0.0f), timerInterval(interval) {}
 
-    // Initialize the encoder with timer setup
+    // Initialize encoder (set up pins)
     void init() override {
-        // Enable timer clock (assuming TIM2)
-        if (timer == TIM2) {
-            RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-        } else if (timer == TIM3) {
-            RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+        pinMode(pinA, INPUT);
+        pinMode(pinB, INPUT);
+        lastTime = millis();  // Initialize the timer
+    }
+
+    void updatePosition() {
+        if (digitalRead(pinA) == digitalRead(pinB)) {
+            position++;
+        } else {
+            position--;
         }
-        
-        // Configure timer in encoder mode
-        timer->CR1 = 0;
-        timer->SMCR = TIM_SMCR_SMS_0 | TIM_SMCR_SMS_1; // Encoder mode 3
-        timer->CCMR1 = 0x01 | (0x01 << 8);             // Input capture on both channels
-        timer->CCER = TIM_CCER_CC1E | TIM_CCER_CC2E;   // Enable capture for both channels
-        timer->ARR = 0xFFFF;                           // Auto-reload max value
-        timer->CNT = 0;                                // Reset the counter
-        timer->CR1 |= TIM_CR1_CEN;                     // Enable timer
     }
 
     // Get current encoder position
     int32_t getPosition() override {
-        int32_t currentCount = (int16_t)(timer->CNT);
-        position = currentCount;
         return position;
     }
 
     // Reset encoder position
     void resetPosition() override {
-        timer->CNT = 0;
         position = 0;
+        lastPosition = 0;
+    }
+
+    // Calculate and return the speed of the encoder (pulses per second)
+    float getSpeed() {
+        unsigned long currentTime = millis();
+        unsigned long timeInterval = currentTime - lastTime;
+        if (timeInterval >= timerInterval) {
+            int32_t positionChange = position - lastPosition;
+            speed = (positionChange * 1000.0) / timeInterval; // Speed in pulses per second
+            lastPosition = position;
+            lastTime = currentTime;
+        }
+
+        return speed; // Return the calculated speed
     }
 };
 
-#endif // TIMEDENCODER_H
+#endif 
--- main.ino
+++ main.ino
@@ -1,30 +1,29 @@
-#include "TimedEncoder.h"
+#include <ros.h>
+#include <std_msgs/Float32.h>
 #include "InterruptEncoder.h"
 
-// Example using TIM2 for TimedEncoder
-TimedEncoder timedEncoder(TIM2);
-
-// Example using pins PA0 and PA1 for InterruptEncoder
+ros::NodeHandle nh;
+std_msgs::Float32 speed_msg;
+ros::Publisher speed_pub("/speed", &speed_msg);
 InterruptEncoder interruptEncoder(PA0, PA1);
+TimedEncoder timedEncoder(PA0, PA1, 100); // 100 ms interval for speed calculation
 
 void setup() {
-  Serial.begin(115200);
-
-  // Initialize both encoders
-  timedEncoder.init();
+  nh.initNode();
+  nh.advertise(speed_pub);
   interruptEncoder.init();
+  timedEncoder.init();
 }
 
 void loop() {
-  // Get the position from the timer-based encoder
-  int32_t timedPosition = timedEncoder.getPosition();
-  Serial.print("Timed Encoder Position: ");
-  Serial.println(timedPosition);
+  timedEncoder.updatePosition();
+  float speed = interruptEncoder.getSpeed();
+  // float speed = timedEncoder.getSpeed();
 
-  // Get the position from the interrupt-based encoder
-  int32_t interruptPosition = interruptEncoder.getPosition();
-  Serial.print("Interrupt Encoder Position: ");
-  Serial.println(interruptPosition);
+  speed_msg.data = speed;
+  speed_pub.publish(&speed_msg);
+
+  nh.spinOnce();
 
   delay(100);
 }
